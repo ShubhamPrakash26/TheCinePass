6aa7d662a4d775eac93d46eb6ee8688be04f4216
Shubham Prakash <prakashshubham26@gmail.com>
2025-06-22 13:31:38 +0530

    Inngest and MongoDB

---
+++ server/server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/db.js';
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inggest/index.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Database connection
+await connectDB();
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+app.use(clerkMiddleware())
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Server is Live!');
+    }
+);
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
